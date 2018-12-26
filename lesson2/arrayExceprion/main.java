fbcaf3f6ac86b0105034b6917e1ee2ce61cb7bc7
ksilar <44767399+ksilerman@users.noreply.github.com>
2018-12-26 15:34:25 +0300

    Update main.java

--- lesson2/arrayExceprion/main.java
+++ lesson2/arrayExceprion/main.java
@@ -1,27 +1,47 @@
-package lesson2.arrayExceprion;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
+package lesson2.arrayException;
 
 public class main {
     public static void main(String[] args) {
 
-        String[] weekDays = {"1", "2", "3" , "4"};
-        Arr(weekDays);
-    }
-    //int i=Integer.parseInt(s);
-    public static void Arr(String[] a){
-        int[] pars = new int[a.length];
-        int sum;
-        for (int i = 0; i < a.length; i++) {
-            pars[i] = Integer.parseInt(a[i]);
-                System.out.println(pars[i]);
+        String[][] weekDays = { {"1","2","3","4"},{"5","6","7","8"}};
+        try {
+            Arr(weekDays);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            System.out.println(e);
         }
-        sum = pars[0]+pars[1]+pars[2]+pars[3];
-        System.out.println(sum);
+    }
+
+    public static void Arr(String a[][]) {
+        int[][] pars = new int[a.length][]; // массив для преобразования
+        int sum = 0;                        // переменная для суммирования
+
+        for (int i = 0; i < a.length; i++)  {
+            pars[i] = new int[a[i].length];
+
+            for (int j = 0; j < a[i].length; j++) {
+                try {
+                pars[i][j] = Integer.parseInt(a[i][j]) ;
+
+                  } catch (NumberFormatException e) {}
+                    System.out.print(pars[i][j] + " ");
+                }
+            }
+
+        try {
+
+            for (int i = 0; i < pars.length; i++) {
+                for (int j = 0; j < pars[i].length; j++) {
+                    sum = sum + pars[i][j];
+                }
+            }
+        }catch (ArithmeticException e) {
+            System.out.println("");
+        }
+        System.out.println("\n" + "----------");
+        System.out.println("Сумма всех элементов = " + sum);
     }
 
 
-}
+    }
 
 
