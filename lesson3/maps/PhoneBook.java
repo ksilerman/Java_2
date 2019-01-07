52bd9c4440f5b937cb479ec18f90db0ef2675c69
ksilerman <ksilar@mail.ru>
2019-01-07 19:09:43 +0300

    выполнена

--- lesson3/maps/PhoneBook.java
+++ lesson3/maps/PhoneBook.java
@@ -1,4 +1,30 @@
 package lesson3.maps;
 
+import java.util.*;
+
 public class PhoneBook {
+    private Map<String, String> pb;
+
+    public PhoneBook() {
+        pb = new TreeMap<>();
+    }
+
+    public void add(String name, String phone) {
+        pb.put(name, phone);
+    }
+
+    List<String> get(String name) {
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, String> entry : pb.entrySet())
+            if (name.equals(entry.getValue()))
+                list.add(entry.getKey());
+        return list;
+    }
+
+    @Override
+    public String toString() {
+        return pb.toString();
+    }
+
+
 }
